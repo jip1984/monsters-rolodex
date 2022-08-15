730047852f359702a1b3bd1aef0dff30949b9003
Jon Prosser <jon.prosser@capgemini.com>
2022-08-15 19:10:53 +0100

    First class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Component } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component() {
+
+  render (){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +25,7 @@ function App() {
       </header>
     </div>
   );
+ }
 }
 
 export default App;
