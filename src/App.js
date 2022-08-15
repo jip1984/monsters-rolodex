633c0b6b1e10d4c5ffb6b2f1e73ded518d2a0a57
Jon Prosser <jon.prosser@capgemini.com>
2022-08-15 19:32:13 +0100

    sorted web pack issue

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import { Component } from 'react'
-
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component() {
+class App extends Component {
 
   render (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello Jon</h1>
+     <button>Change Name</button>
     </div>
   );
- }
+
 }
+}
+
 
 export default App;
